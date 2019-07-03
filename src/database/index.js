1215b2c1da3e806e4b2fcfac47dbd4f9c9153983
Diogenes Marcato Crespo <diogenesmarcato@gmail.com>
2019-07-02 23:05:28 -0300

    sem config

--- src/database/index.js
+++ src/database/index.js
@@ -5,7 +5,7 @@ import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
 
-import databaseConfig from '../config/database';
+import databaseConfig from '../config/env/database';
 
 const models = [User, File, Appointment];
 
