08b07483249120558433d23d38a29e8da6c5e8a2
senegoid <diogenesmarcato@gmail.com>
2019-07-03 13:14:51 -0300

    Com cancelamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup'
-import { startOfHour, parseISO, isBefore, format } from 'date-fns'
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns'
 import pt from 'date-fns/locale/pt'
 
 import User from '../models/User'
@@ -92,6 +92,28 @@ class AppointmentController {
     })
 
     return res.json(appointment)
+  }
+  async delete (req, res) {
+    const appointment = await Appointment.findByPk(req.params.id)
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: 'Você não pode cancelar este agendamento.'
+      })
+    }
+
+    const dateWithSub = subHours(appointment.date, 2)
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'Você só pode cancelar o agendamento duas horas antes.'
+      })
+    }
+
+    appointment.canceled_at = new Date()
+
+    await appointment.save()
+
+    res.json(appointment)
   }
 }
 
